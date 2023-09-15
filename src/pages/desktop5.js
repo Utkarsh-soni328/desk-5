e9036b8c701f72483b86d87a54b9e9a4173d3a0f
Utkarsh-soni328 <124017589+Utkarsh-soni328@users.noreply.github.com>
2023-09-15 12:32:38 +0530

    Add files via upload

---
+++ src/pages/desktop5.js
@@ -0,0 +1,189 @@
+const Desktop5 = () => {
+  return (
+    <div className="relative bg-white w-full h-[1400px] overflow-hidden text-center text-xl text-white font-inter">
+      <div className="absolute top-[220px] left-[0px] bg-lightskyblue w-[1440px] h-[713px]" />
+      <div className="absolute top-[0px] left-[0px] w-[1440px] h-[220px] text-black">
+        <div className="absolute top-[0px] left-[0px] bg-royalblue w-[1440px] h-[220px]" />
+        <div className="absolute top-[77px] left-[43px] bg-gainsboro w-[291px] h-10" />
+        <div className="absolute top-[91px] left-[408px] w-[866px] h-[33px]">
+          <b className="absolute top-[6.29px] left-[584.99px] inline-block w-[143.24px] h-[26.71px]">
+            Financial Aids
+          </b>
+          <b className="absolute top-[3.14px] left-[358.65px] inline-block w-[103.88px] h-[26.71px]">
+            Lab
+          </b>
+          <b className="absolute top-[0px] left-[211.03px] inline-block w-[154.17px] h-[29.86px]">
+            Get Resources
+          </b>
+          <b className="absolute top-[0px] left-[107.16px] inline-block w-[103.88px] h-[26.71px]">
+            Learn
+          </b>
+          <b className="absolute top-[0px] left-[0px] inline-block w-[103.88px] h-[26.71px]">
+            About Us
+          </b>
+          <b className="absolute top-[3.14px] left-[450.49px] inline-block w-[117px] h-[26.71px]">
+            Job Portal
+          </b>
+          <b className="absolute top-[6.29px] left-[747.91px] inline-block w-[118.09px] h-[26.71px]">
+            Contact Us
+          </b>
+        </div>
+        <div className="absolute top-[165px] left-[1289px] text-base inline-block w-[141px] h-8">
+          Login/signup
+        </div>
+        <img
+          className="absolute h-[52.27%] w-[8.06%] top-[15.45%] right-[1.66%] bottom-[32.27%] left-[90.28%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/group.svg"
+        />
+      </div>
+      <div className="absolute top-[933px] left-[0px] w-[1440px] h-[467px] text-left text-3xl">
+        <div className="absolute top-[0px] left-[0px] bg-royalblue w-[1440px] h-[467px]" />
+        <div className="absolute top-[360px] left-[0px] bg-gray w-[1440px] h-[107px]" />
+        <div className="absolute top-[42px] left-[826px] w-[452px] h-[307px]">
+          <div className="absolute top-[50px] left-[35px] inline-block w-[417px] h-[104px]">
+            For any queries regarding the NPTEL website,availability of courses
+            or issues in accessing courses,please contact
+          </div>
+          <div className="absolute top-[0px] left-[0px] text-5xl text-center inline-block w-[194px] h-[38px]">
+            Contact Us:
+          </div>
+          <div className="absolute top-[168px] left-[28px] text-[inherit] inline-block w-[415px] h-[139px]">
+            {`NPTEL Administrator,IC & SR, 3rd floorIIT Madras, Chennai - 600036Tel : `}
+            <a
+              className="text-[inherit]"
+              href="tel:044 2257 5905"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                (044) 2257 5905
+              </span>
+            </a>
+            {`, `}
+            <a
+              className="text-[inherit]"
+              href="tel:044 2257 5908"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                (044) 2257 5908
+              </span>
+            </a>
+            {`, `}
+            <a className="text-[inherit]" href="tel:9363218521" target="_blank">
+              <span className="[text-decoration:underline]">9363218521</span>
+            </a>
+            {` (Mon-Fri 9am-6pm)Email : `}
+            <a
+              className="text-[inherit]"
+              href="mailto:support@nptel.iitm.ac.in"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                support@nptel.iitm.ac.in
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="absolute top-[42px] left-[162px] w-[194px] h-64 text-5xl">
+          <div className="absolute top-[0px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[218px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[173px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[128px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[83px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+        </div>
+        <b className="absolute top-[401px] left-[140px] text-xl inline-block text-center w-[317px] h-[25px]">
+          © - 2022 All rights reserved
+        </b>
+      </div>
+      <div className="absolute top-[364px] left-[194px] w-[1052px] h-[396px] text-left text-15xl">
+        <div className="absolute top-[0px] left-[0px] w-[1052px] h-[69px]">
+          <div className="absolute top-[0px] left-[0px] rounded-25xl bg-darkslateblue-100 w-[1052px] h-[69px]" />
+          <div className="absolute top-[11px] left-[30px] w-[996px] h-[46px]">
+          <a href="https://astonishing-stardust-9bd411.netlify.app/" className="text-white"><div className="absolute top-[0px] left-[46px] inline-block w-[276px] h-[46px]">
+              Basics
+            </div></a>
+            <div className="absolute top-[0px] left-[0px] inline-block w-10 h-[31px]">
+              1.
+            </div>
+            <div className="absolute top-[0px] left-[916px] inline-block w-20 h-[26px]">
+              1/25
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[327px] left-[0px] w-[1052px] h-[69px]">
+          <div className="absolute top-[0px] left-[0px] rounded-25xl bg-darkslateblue-100 w-[1052px] h-[69px]" />
+          <div className="absolute top-[11px] left-[30px] w-[996px] h-[46px]">
+            <div className="absolute top-[0px] left-[46px] inline-block w-[276px] h-[46px]">
+              Projectile
+            </div>
+            <div className="absolute top-[0px] left-[0px] inline-block w-10 h-[31px]">
+              4.
+            </div>
+            <div className="absolute top-[0px] left-[916px] inline-block w-20 h-[26px]">
+              1/25
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[222px] left-[0px] w-[1052px] h-[69px]">
+          <div className="absolute top-[0px] left-[0px] rounded-25xl bg-darkslateblue-100 w-[1052px] h-[69px]" />
+          <div className="absolute top-[11px] left-[30px] w-[996px] h-[46px]">
+            <div className="absolute top-[0px] left-[46px] inline-block w-[276px] h-[46px]">
+              3D
+            </div>
+            <div className="absolute top-[0px] left-[0px] inline-block w-10 h-[31px]">
+              3.
+            </div>
+            <div className="absolute top-[0px] left-[916px] inline-block w-20 h-[26px]">
+              1/25
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[111px] left-[0px] w-[1052px] h-[69px]">
+          <div className="absolute top-[0px] left-[0px] rounded-25xl bg-darkslateblue-100 w-[1052px] h-[69px]" />
+          <div className="absolute top-[11px] left-[30px] w-[996px] h-[46px]">
+            <div className="absolute top-[0px] left-[46px] inline-block w-[276px] h-[46px]">
+              2D
+            </div>
+            <div className="absolute top-[0px] left-[0px] inline-block w-10 h-[31px]">
+              2.
+            </div>
+            <div className="absolute top-[0px] left-[916px] inline-block w-20 h-[26px]">
+              1/25
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[243px] left-[53px] w-[1306px] h-[79px] text-smi">
+        <div className="absolute top-[0px] left-[0px] text-[48px] text-black inline-block w-[293px] h-[66px]">
+          Kinematics
+        </div>
+        <div className="absolute top-[66px] left-[92px] w-[1179px] h-[13px]">
+          <div className="absolute top-[2px] left-[0px] bg-white w-[1124px] h-[11px]" />
+          <div className="absolute top-[2px] left-[0px] bg-tomato w-[922px] h-[11px]" />
+          <div className="absolute top-[0px] left-[1132px] inline-block w-[47px] h-[11px]">
+            88%
+          </div>
+        </div>
+        <div className="absolute top-[18px] left-[1065px] w-[241px] h-[30px]">
+          <div className="absolute top-[0px] left-[0px] rounded-lg bg-tomato w-[241px] h-[30px]" />
+          <b className="absolute top-[8px] left-[22px] inline-block w-[196px] h-3.5">
+            Download Notes
+          </b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop5;
